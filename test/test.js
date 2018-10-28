00f100f05fae6bb4d93f73d2d4065bcf1b574835
Richard <xrouter@gmail.com>
2018-10-28 22:41:12 +0000

    Work on tests

--- test/test.js
+++ test/test.js
@@ -1,8 +1,35 @@
-var assert = require('assert');
-describe('Queue', function() {
-	describe('#indexOf()', function() {
-		it('should return -1 when the value is not present', function() {
-			assert.equal([1,2,3].indexOf(4), -1);
+var expect = chai.expect;
+
+describe('#mohocore.data.neun', function () {
+	describe("return", function () {
+		it('Should return true when empty', function () {
+			expect(mohocore.data.neun('')).to.equal(true);
 		});
+
+		it('Should return true when undefined CASE1', function () {
+			expect(mohocore.data.neun(undefined)).to.equal(true);
+		});
+
+		it('Should return true when undefined CASE2', function () {
+			expect(mohocore.data.neun()).to.equal(true);
+		});
+
+		it('Should return true when -1', function () {
+			expect(mohocore.data.neun(-1)).to.equal(true);
+		});
+
+		it('Should return true when null', function () {
+			expect(mohocore.data.neun(null)).to.equal(true);
+		});
+
+		it('Should return true when "null"', function () {
+			expect(mohocore.data.neun("null")).to.equal(true);
+		});
+
+
+		it('Should return false when "foo"', function () {
+			expect(mohocore.data.neun("foo")).to.equal(false);
+		});
+
 	});
 });
