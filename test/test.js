37364f2568658d471b542071b7ee0309f4b63552
Richard Reynolds <richard@nautguide.com>
2018-10-29 15:19:27 +0000

    tidy up

--- test/test.js
+++ test/test.js
@@ -1,5 +1,9 @@
 var expect = chai.expect;
 
+/**
+ *  Check the queue object is alive and has the required properties
+ */
+
 describe('#Queue function', function () {
 	describe("Exist", function () {
 		it('Should be in window', function () {
@@ -10,6 +14,31 @@ describe('#Queue function', function () {
 	describe("Available", function () {
 		it('Should have a status set', function () {
 			expect(window.queue).to.have.property('status');
+		});
+
+	});
+});
+
+/**
+ *  Tests for command processor
+ */
+
+describe('#Queue Command Parse', function () {
+	describe("@templates.render({\"targetId\":\"content\",\"template\":\"basic\"},{\"queueRun\":\"Instant\"});", function () {
+		it('Should have a status set', function () {
+			expect(window.queue.commandParse('templates.render({"targetId":"content","template":"basic"},{"queueRun":"Instant"});'))
+				.to.deep.equal({
+				"command":"render",
+				"json":{
+					"targetId": "content",
+					"template": "basic"
+				},
+				"options":{
+					"queueRun": "Instant"
+				},
+				"queueable":"templates",
+				"state":0
+				});
 		});
 
 	});
