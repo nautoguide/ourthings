45458d44eec784cd89ce82881593bdc8245ef2bb
Richard <xrouter@gmail.com>
2018-11-22 22:28:00 +0000

    Updated API, new Element function and perm memory support

--- src/ourthings/Queueable/Elements.js
+++ src/ourthings/Queueable/Elements.js
@@ -33,6 +33,27 @@ export default class Elements extends Queueable {
 			self.finished(pid,self.queue.DEFINE.FIN_WARNING,'Could not add class ['+json.class+'] to ['+json.targetId+']');
 		}
 	}
+
+	/**
+	 * Remove a class to a dom element
+	 * @param {number} pid - Process ID
+	 * @param {object} json - queue arguments
+	 * @param {string} json.targetId - Dom target
+	 * @param {string} json.class - Name of class to remove
+	 */
+	removeClass(pid,json) {
+		let self=this;
+		let element=self.queue.getElement(json.targetId);
+		self.set(pid,json);
+
+		if(element!==false) {
+			element.classList.remove(json.class);
+			self.finished(pid,self.queue.DEFINE.FIN_OK);
+		} else {
+			self.finished(pid,self.queue.DEFINE.FIN_WARNING,'Could not remove class ['+json.class+'] to ['+json.targetId+']');
+		}
+	}
+
     /**
      * Set the HTML of an element
      * @param {number} pid - Process ID
--- src/ourthings/Queueable/Internals.js
+++ src/ourthings/Queueable/Internals.js
@@ -15,14 +15,39 @@ import Queueable from "../Queueable";
 export default class Internals extends Queueable {
 
 	/**
-	 * Render a template into the dom using the queues templateProcessor
+	 * Execute a prepared queue
 	 * @param {int} pid - process ID
 	 * @param {object} json - queue arguments
 	 * @param {string} json.name - prepared queue to call
+	 * @param {object} [json.json] - New arguments to send to queue
 	 */
 	execute(pid,json) {
 		let self=this;
-		self.queue.execute(json.name);
+		self.queue.execute(json.name,json.json);
 		self.finished(pid,self.queue.DEFINE.FIN_OK);
+	}
+
+	/**
+	 * Check a statement and run prepared queue if its true
+	 * @param {int} pid - process ID
+	 * @param {object} json - queue arguments
+	 * @param {string} json.statement - statement to check
+	 * @param {string} json.name - prepared queue to call
+	 * @param {object} [json.json] - New arguments to send to queue
+	 */
+	ifqueue(pid,json) {
+		let self=this;
+		if(eval(json.statement)) {
+			self.queue.execute(json.name,json.json);
+
+		}
+		self.finished(pid,self.queue.DEFINE.FIN_OK);
+	}
+
+	setMemory(pid,json) {
+		let self=this;
+		self.queue.setMemory(json.name,json.value,json.mode);
+		self.finished(pid,self.queue.DEFINE.FIN_OK);
+
 	}
 }
