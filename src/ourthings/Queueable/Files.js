b2b1e6f3c5ffadb7ff313d32c772a48156c4fd42
Richard Reynolds <richard@nautguide.com>
2019-02-26 16:12:51 +0000

    Work in progress files (needs docs / testing)

--- src/ourthings/Queueable/Files.js
+++ src/ourthings/Queueable/Files.js
@@ -27,11 +27,15 @@ export default class Files extends Queueable {
 		let files=element.files;
 		if(files&&files[0]) {
 			let reader = new FileReader();
-			reader.readAsBinaryString(files[0], "UTF-8");
+			reader.readAsDataURL(files[0]);
 			reader.onload = function (evt) {
-				self.set(pid,window.btoa(evt.target.result));
+				if(json.previewId) {
+					let preview=self.queue.getElement(json.previewId);
+					preview.src=evt.target.result;
+				} else {
+					self.set(pid, evt.target.result);
+				}
 				self.finished(pid,self.queue.DEFINE.FIN_OK);
-
 			};
 			reader.onerror = function (evt) {
 				// TODO Write me!
@@ -42,6 +46,68 @@ export default class Files extends Queueable {
 			};
 		}
 
+	}
+
+	filePreview(pid,json) {
+		let self=this;
+		let element=self.queue.getElement(json.targetId);
+		element.addEventListener('change', function () {
+			let file=this.files[0];
+			let reader = new FileReader();
+			reader.readAsDataURL(file);
+			reader.onload = function (evt) {
+				if(json.previewId) {
+					let preview=self.queue.getElement(json.previewId);
+					preview.src=evt.target.result;
+				} else {
+					self.set(pid, evt.target.result);
+				}
+				self.finished(pid,self.queue.DEFINE.FIN_OK);
+			};
+			reader.onerror = function (evt) {
+				// TODO Write me!
+				console.log('error');
+				debugger;
+				self.finished(pid,self.queue.DEFINE.FIN_ERROR);
+
+			};
+		});
+	}
+
+	putFileToS3(pid,json) {
+		let self=this;
+		json.contentType=json.contentType||'image/png';
+		let headers=json.headers||{};
+		headers['Content-Type']=json.contentType;
+		let element=self.queue.getElement(json.targetId);
+		let files=element.files;
+		fetch(json.url, {
+			headers: headers,
+			method: 'PUT',
+			body: files[0]
+		})
+			.then(response => self.queue.handleFetchErrors(response))
+			.then(function(response) {
+				switch(json.contentType) {
+					case 'application/json':
+						return response.json();
+					default:
+						return response.text();
+				}
+			})
+			.then(function (response) {
+				/*
+				 * Convert the response to json and start the loader
+				 */
+				self.set(pid,response);
+				self.finished(pid,self.queue.DEFINE.FIN_OK);
+
+			})
+			.catch(function (error) {
+				console.info(self.queue.DEFINE.CONSOLE_LINE);
+				console.error('Error:', error);
+				console.info("Warning this error is probably fatal as I have no templates to load")
+			});
 
 	}
 }
