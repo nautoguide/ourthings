f18a09a7e261b1a1b75fc96283488d7d2b844295
Richard Reynolds <xrouter@gmail.com>
2019-11-13 13:13:22 +0000

    Added markdown options hook

--- src/ourthings/Queueable/Markdown.js
+++ src/ourthings/Queueable/Markdown.js
@@ -22,12 +22,17 @@ export default class Markdown extends Queueable {
 	 * @param {object} json - queue arguments
 	 * @param {string} json.md - Markdown string
 	 * @param {string} json.name - memory name to set with the html
+	 * @param {string} json.options - Options to pass to showdown see: https://github.com/showdownjs/showdown/wiki/Showdown-options
+	 * @example markdown.makeHtml({"md":"{{!memory.select.value.properties.description}}","options":{"openLinksInNewWindow":true}});
+
 	 */
 	makeHtml(pid,json) {
 		let options=Object.assign({
 			"name":"html",
+			"options":{}
 		},json);
-		const converter = new showdown.Converter();
+
+		const converter = new showdown.Converter(options.options);
 		let html=converter.makeHtml(json.md);
 		this.queue.setMemory(options.name,html,"Session");
 		this.finished(pid,this.queue.DEFINE.FIN_OK);
