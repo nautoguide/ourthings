39e671be9241b8ec9a69802a184caba400acac95
Richard Reynolds <xrouter@gmail.com>
2020-05-29 07:14:38 +0100

    Working menu code

--- src/ourthings/Queueable/Menu.js
+++ src/ourthings/Queueable/Menu.js
@@ -9,7 +9,7 @@ import Queueable from "../Queueable";
  * @author Richard Reynolds richard@nautoguide.com
  *
  * @example
- * menu.init({"targetId":"content"});
+ * menu.init({"targetId":"menu1"});
  *
  */
 class Menu extends Queueable {
@@ -18,14 +18,40 @@ class Menu extends Queueable {
 	 * Render a template into the dom using the queues templateProcessor
 	 * @param {number} pid - Process ID
 	 * @param {object} json - queue arguments
-	 * @param {string} json.template - dom id of template to use
-	 * @param {string} [json.target] - dom id of render target
-	 * @param {string} [json.mode] - "insert|append"
+	 * @param {string} json.targetId - dom id of menu
 	 * @example
-	 * templates.render({"targetId":"content","template":"basic"});
+	 * menu.init({"targetId":"menu1"});
 	 */
-	init(pid,json) {
+	initMenu(pid,json) {
 		let self=this;
+		let options = Object.assign({
+			"classModifiers":[
+				{"targetId":".main-menu","class":"open-this-menu"},
+				{"targetId":".page-header","clas":"open-search"}
+			],
+			"menuTop":"#menuTop"
+		}, json);
+		const element=this.queue.getElement(options.targetId);
+
+		element.addEventListener("click", function (e) {
+			const menuTop=self.queue.getElement(options.menuTop);
+			if(menuTop.getAttribute('aria-expanded')==='true') {
+				// Close
+				for (let i in options.classModifiers) {
+					const modElement = self.queue.getElement(options.classModifiers[i].targetId);
+					modElement.classList.remove(options.classModifiers[i].class);
+				}
+				menuTop.setAttribute('aria-expanded','false')
+			} else {
+				// Open
+				for (let i in options.classModifiers) {
+					const modElement = self.queue.getElement(options.classModifiers[i].targetId);
+					modElement.classList.add(options.classModifiers[i].class);
+				}
+				menuTop.setAttribute('aria-expanded','true')
+			}
+		});
+
 		self.finished(pid,self.queue.DEFINE.FIN_OK);
 	}
 }
