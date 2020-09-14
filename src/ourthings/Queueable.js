df7792a084cd0371a780e08a9183511231d0ea73
Richard Reynolds <xrouter@gmail.com>
2020-09-14 11:46:14 +0100

    Added a generalError queue & generalErrorMessage memory when running a Queueable.js to enable trapping general errors

--- src/ourthings/Queueable.js
+++ src/ourthings/Queueable.js
@@ -66,6 +66,10 @@ class Queueable {
 			try {
 				self[command](pid, json);
 			} catch(e) {
+				/*
+				 * In an error state, we will fire up the debugger is they have console open
+				 */
+				console.log(e);
 				self.queue.setMemory('generalErrorMessage',e,"Session");
 				self.queue.execute('generalError');
 				self.queue.finished(pid,self.queue.DEFINE.FIN_ERROR,`Queue [${command}] errored: ${e}`);
