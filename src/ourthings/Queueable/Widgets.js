8bc8c34dfef427e0c9ad454d7e63be723efaeadc
Richard Reynolds <xrouter@gmail.com>
2020-10-26 15:31:47 +0000

    Added eval method to Internals.js. Updated Widgets.js to support disabled pages and running queues on page movements

--- src/ourthings/Queueable/Widgets.js
+++ src/ourthings/Queueable/Widgets.js
@@ -23,24 +23,54 @@ class Widgets extends Queueable {
 	 * @param {string} [json.maxClass] - class to use for max
 	 * @param {string} [json.prevTarget] - class to use for prev
 	 * @param {string} [json.nextTarget] - class to use for next
+	 * @param {string} [json.queuePrefix] - prefix to use for queue runner
+	 * @param {array} [json.disabledPages] - list of disabled pages
 	 * @example
 	 * widgets.initPage();
 	 */
-	initPage(pid,json) {
+	initPage(pid, json) {
 		let options = Object.assign({
 			"startPage": 1,
 			"maxClass": "max",
 			"prevTarget": ".page-left",
 			"nextTarget": ".page-right",
-			"totalPages":100
+			"totalPages": 100,
+			"disabledPages":[],
+			"queuePrefix":undefined
 		}, json);
 		this.queue.setMemory("currentPage", parseInt(options.startPage), "Session");
-		this.pageMaxClass=options.maxClass;
-		this.pagePrevTarget=options.prevTarget;
-		this.pageNextTarget=options.nextTarget;
-		this.pageTotal=options.totalPages;
+		this.pageMaxClass = options.maxClass;
+		this.pagePrevTarget = options.prevTarget;
+		this.pageNextTarget = options.nextTarget;
+		this.pageTotal = options.totalPages;
+		this.disabledPages = options.disabledPages;
+		this.queuePrefix = options.queuePrefix;
+
+		if (parseInt(options.startPage) <= 1)
+			queue.getElement(this.pagePrevTarget).classList.add(this.pageMaxClass);
+
+		if (parseInt(options.startPage) >= this.pageTotal)
+			queue.getElement(this.pageNextTarget).classList.add(this.pageMaxClass);
+
+		if(this.queuePrefix)
+			queue.execute(`${this.queuePrefix}Page${memory.currentPage.value}`);
+
 		this.finished(pid, this.queue.DEFINE.FIN_OK);
 	}
+
+	/**
+	 * Disable pages
+	 * @param {number} pid - Process ID
+	 * @param {object} json - queue arguments
+	 * @param {array} [json.disabledPages] - list of disabled pages
+	 * @example
+	 * widgets.disablePages({"disabledPages":[4,5,6]});
+	 */
+	disablePages(pid,json) {
+		this.disabledPages = json.disabledPages;
+		this.finished(pid, this.queue.DEFINE.FIN_OK);
+	}
+
 	/**
 	 * Set the current page
 	 * @param {number} pid - Process ID
@@ -50,23 +80,31 @@ class Widgets extends Queueable {
 	 * @example
 	 * widgets.setPage({"targetId":"content","template":"basic"});
 	 */
-	setPage(pid,json) {
-		let newPage=memory.currentPage.value+parseInt(json.incr);
-		if(json.page!==undefined)
-			newPage=parseInt(json.page);
-		if(newPage>=1&&newPage<=this.pageTotal)
-			memory.currentPage.value=newPage;
-		if(newPage===0) {
+	setPage(pid, json) {
+		let newPage = memory.currentPage.value + parseInt(json.incr);
+		if(this.disabledPages.length>0&&this.disabledPages.indexOf(newPage)!==-1) {
+			for(;newPage<this.pageTotal&&newPage>1;newPage+=parseInt(json.incr)) {
+				if(this.disabledPages.indexOf(newPage)==-1)
+					break;
+			}
+		}
+		if (json.page !== undefined)
+			newPage = parseInt(json.page);
+		if (newPage >= 1 && newPage <= this.pageTotal)
+			memory.currentPage.value = newPage;
+		if (newPage <= 1) {
 			queue.getElement(this.pagePrevTarget).classList.add(this.pageMaxClass);
 		} else {
 			queue.getElement(this.pagePrevTarget).classList.remove(this.pageMaxClass);
 		}
 
-		if(newPage===this.pageTotal) {
+		if (newPage >= this.pageTotal) {
 			queue.getElement(this.pageNextTarget).classList.add(this.pageMaxClass);
 		} else {
 			queue.getElement(this.pageNextTarget).classList.remove(this.pageMaxClass);
 		}
+		if(this.queuePrefix)
+			queue.execute(`${this.queuePrefix}Page${memory.currentPage.value}`);
 		this.finished(pid, this.queue.DEFINE.FIN_OK);
 	}
 }
